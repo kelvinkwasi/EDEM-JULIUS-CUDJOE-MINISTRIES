d7a90088f8615483acbf1ce3ebd9b38c06653237
kelvin kwasi <kkwasi@kelvins-MacBook-Pro.local>
2024-02-15 18:34:30 +0000

    First commit

---
+++ scipt.js
@@ -0,0 +1,10 @@
+const button = document.querySelector ('.button');
+const textsInputs = document.querySelectorAll ('input[type="text"]');
+
+button .onclick = signUp;
+
+function signUp () {
+    console.log(' Sign Up');
+}
+
+function
